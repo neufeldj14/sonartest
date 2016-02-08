bc318596786b1ec8cd8a5ade3a35379eb29764c5
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-02-08 13:53:36 +0100

    Fix creation of DB in sonar-db

--- run-db-unit-tests.sh
+++ run-db-unit-tests.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 ORCHESTRATOR_CONFIG_URL=$1
 shift
 
-mvn verify -pl :sonar-db -Dorchestrator.configUrl=$ORCHESTRATOR_CONFIG_URL -B -e -V $*
+mvn org.sonarsource.orchestrator:orchestrator-maven-plugin:3.10.1:create-db verify \
+  -pl :sonar-db \
+  -Dorchestrator.configUrl=$ORCHESTRATOR_CONFIG_URL \
+  -B -e -V $*
